c3d3f6712aef2b58342a8cc48036d039095df339
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-28 19:05:07 +0100

    added api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json(compact('projects'));
+});
+
+Route::get('/projects', function () {
+    $projects = Project::all();
+    return response()->json($projects);
 });
