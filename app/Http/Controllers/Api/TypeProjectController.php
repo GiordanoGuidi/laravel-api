7090ff339fcd5c89ad124583c8d68c6f063e4d8f
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-03 19:26:51 +0200

    added TypeProjectController

---
+++ app/Http/Controllers/Api/TypeProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class TypeProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function __invoke()
+    {
+        //
+    }
+}
