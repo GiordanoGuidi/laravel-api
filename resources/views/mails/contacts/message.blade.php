bb35f6f7a0e79ad801c7e648745d74bec6f7b0cc
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-05 23:15:21 +0200

    added file message.blade.php

---
+++ resources/views/mails/contacts/message.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{env('APP_NAME')}}</title>
+    <style>
+        body{
+            background-color: blueviolet;
+            text-align: center;
+            font-family: Verdana, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <p>E' stato inviato un messaggio</p>
+</body>
+</html>
