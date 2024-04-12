38a299a483e6683ffe29495be51e5c1a5194fb3d
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-12 17:45:47 +0200

    added validation into ContactController

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -6,18 +6,40 @@ use App\Http\Controllers\Controller;
 use App\Mail\ContactMessageMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
     public function message(Request $request)
     {
-        //Prendo tutti i data passati nella request
+        //Prendo tutti i dati passati nella request
         $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'email' => 'required|email',
+            'subject' => 'required|string',
+            'message' => 'required|string',
+        ], [
+            'email.required' => 'Il campo email è obbligatorio',
+            'email.email' => 'La mail non è valida',
+            'subject.required' => 'L\'oggetto della mail è obbligatorio',
+            'message.required' => 'Il messaggio della mail è obbligatorio',
+        ]);
+
+        /*Facciamo restituire al validatore soltannto il primo
+        messaggio di errore per ogni campo sbagliato*/
+        if ($validator->fails()) {
+            $errors = [];
+            foreach ($validator->errors()->messages() as $field => $messages) {
+                $errors[$field] = $messages[0];
+            }
+            return response()->json(compact('errors'), 422);
+        }
 
         $mail = new ContactMessageMail(
             subject: $data['subject'],
-            sender: $data['sender'],
-            content: $data['content'],
+            sender: $data['email'],
+            content: $data['message'],
         );
         Mail::to(env('MAIL_TO_ADDRESS'))->send($mail);
         //Tutto ok no content
